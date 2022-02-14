cfaa53663a4cc4c60013a4c5d681e0efaa4ab36f
Athul Saji <77442224+Athul-Saji@users.noreply.github.com>
2022-02-14 23:58:31 +0530

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -26,51 +26,16 @@ public class Calculator {
             btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
             btnPoint, btnEqual, btnRoot, btnPower, btnLog;
 
-    private char opt = ' '; // Save the operator
-    private boolean go = true; // For calculate with Opt != (=)
-    private boolean addWrite = true; // Connect numbers in display
-    private double val = 0; // Save the value typed for calculation
+    private char opt = ' '; 
+    private boolean go = true; 
+    private boolean addWrite = true; 
+    private double val = 0; 
 
-    /*
-        Mx Calculator: 
-        X = Row
-        Y = Column
-    
-        +-------------------+
-        |   +-----------+   |   y[0]
-        |   |           |   |
-        |   +-----------+   |
-        |                   |
-        |   C  <-   %   /   |   y[1]
-        |   7   8   9   *   |   y[2]
-        |   4   5   6   -   |   y[3]
-        |   1   2   3   +   |   y[4]
-        |   .   0     =     |   y[5]
-        +-------------------+
-         x[0] x[1] x[2] x[3]
-    
-    */
-    
-    /*    
-        +-------------------+
-        |   +-----------+   |   y[0]
-        |   |           |   |
-        |   +-----------+   |
-        |                   |
-        |   0   1   1   3   |   y[1]
-        |   4   5   6   7   |   y[2]
-        |   8   9   10  11  |   y[3]
-        |   12  13  14  15  |   y[4]
-        |   16  17    18    |   y[5]
-        +-------------------+
-         x[0] x[1] x[2] x[3]
-    
-    */
 
     public Calculator() {
         window = new JFrame("Calculator");
         window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        window.setLocationRelativeTo(null); // Move window to center
+        window.setLocationRelativeTo(null); 
 
         comboTheme = initCombo(new String[]{"Simple", "Colored", "DarkTheme"}, 230, 30, "Theme", themeSwitchEventConsumer);
 
